8b903f4e51064db2cb174cde9e0200f5c89bce26
Jacquin Moraga <moragaj@hawaii.edu>
2025-09-09 14:29:03 -1000

    My work for in person class

---
+++ Week_03/Scripts/Week_03_script.R
@@ -0,0 +1,25 @@
+
+## Load Libraries ####
+library(ggplot2)
+library(palmerpenguins)
+library(tidyverse)
+
+
+
+## Glimpse Data ####
+glimpse(penguins)
+
+
+ggplot(data = penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+                     aplha = flipper_length_mm)) +
+geom_point()+ 
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, Gentoo",
+       x = "Bill depth (mm)", y = "Bill length (mm)",
+       color = "Species",
+       caption = "Source: Palmer Station")+ 
+  scale_color_viridis_d()
+
